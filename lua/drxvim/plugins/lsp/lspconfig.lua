2004e8689a15bea5396114195a743747a586aadc
arpit <arpit2003y@gmail.com>
2024-06-10 21:40:59 +0530

    chore(diagnostics): configuration_changes

--- lua/drxvim/plugins/lsp/lspconfig.lua
+++ lua/drxvim/plugins/lsp/lspconfig.lua
@@ -35,7 +35,7 @@ vim.diagnostic.config({
 	underline = true,
 	update_in_insert = false,
 	severity_sort = true,
-	signs = { text = { [1] = "->", [2] = " ", [3] = " ", [4] = "󰛨 " } },
+	signs = { text = { [1] = " ", [2] = " ", [3] = " ", [4] = "󰛨 " } },
 	float = {
 		focusable = false,
 		suffix = "",
