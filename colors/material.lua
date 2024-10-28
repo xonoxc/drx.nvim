0de96522e34bed165008fed07d9141a16f67e36c
arpit <arpit2003y@gmail.com>
2024-10-28 20:43:52 +0530

    fix(material_theme):color_changes_material_theme

--- colors/material.lua
+++ colors/material.lua
@@ -1,3 +1,3 @@
-vim.g.currentTheme = "material_darker"
+vim.g.currentTheme = "material"
 require("plenary.reload").reload_module("drxvim.themes")
 require("drxvim.themes").load()
---
+++ lua/drxvim/themes/schemes/material.lua
@@ -0,0 +1,57 @@
+local M = {}
+
+function M.get_colors()
+	return {
+
+		white = "#c0caf5",
+		darker_black = "#1b1e2b",
+		black = "#1e222e",
+		black2 = "#252935",
+		one_bg = "#2a2e3f",
+		one_bg2 = "#363b4f",
+		one_bg3 = "#3e4358",
+		grey = "#495162",
+		grey_fg = "#545b70",
+		grey_fg2 = "#5f677d",
+		light_grey = "#6b7385",
+		red = "#f7768e",
+		baby_pink = "#DE8C92",
+		pink = "#ff75a0",
+		line = "#32364a",
+		green = "#9ece6a",
+		vibrant_green = "#73daca",
+		nord_blue = "#80a8fd",
+		blue = "#7aa2f7",
+		yellow = "#e0af68",
+		sun = "#EBCB8B",
+		purple = "#bb9af7",
+		dark_purple = "#9d7cd8",
+		teal = "#1abc9c",
+		orange = "#ff9e64",
+		cyan = "#7dcfff",
+		statusline_bg = "#22263a",
+		lightbg = "#2f3447",
+		pmenu_bg = "#7aa2f7",
+		folder_bg = "#7aa2f7",
+
+		-- base colors
+		base00 = "#212121",
+		base01 = "#292929",
+		base02 = "#303030",
+		base03 = "#383838",
+		base04 = "#404040",
+		base05 = "#DFF0F0",
+		base06 = "#E6F7F7",
+		base07 = "#eeffff",
+		base08 = "#b0bec5",
+		base09 = "#f78c6c",
+		base0A = "#ffcb6b",
+		base0B = "#c3e88d",
+		base0C = "#c3e88d",
+		base0D = "#82aaff",
+		base0E = "#c792ea",
+		base0F = "#f07178",
+	}
+end
+
+return M
