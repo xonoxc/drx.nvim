aea3c3de7d9c3a138e921b395fc6b14cbca0f2cb
arpit <arpit2003y@gmail.com>
2025-01-05 08:50:30 +0530

    chore(snip):added_additional_snippets

--- lua/drxvim/plugins/init.lua
+++ lua/drxvim/plugins/init.lua
@@ -359,16 +359,16 @@ local plugins = {
 			})
 		end,
 	},
-	-- {
-	-- 	"Exafunction/codeium.nvim",
-	-- 	dependencies = {
-	-- 		"nvim-lua/plenary.nvim",
-	-- 		"hrsh7th/nvim-cmp",
-	-- 	},
-	-- 	config = function()
-	-- 		require("codeium").setup({})
-	-- 	end,
-	-- },
+	{
+		"Exafunction/codeium.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"hrsh7th/nvim-cmp",
+		},
+		config = function()
+			require("codeium").setup({})
+		end,
+	},
 }
 
 local custom_path = vim.fn.stdpath("config") .. "/lua/custom"
