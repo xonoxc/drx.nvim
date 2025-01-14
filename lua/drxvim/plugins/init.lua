f4fffc27ec9d9f7759a28ad1ceb00b034386b144
arpit <arpit2003y@gmail.com>
2025-01-14 22:04:37 +0530

    feat(plugins):added_copilot

--- lua/drxvim/plugins/init.lua
+++ lua/drxvim/plugins/init.lua
@@ -359,15 +359,18 @@ local plugins = {
 			})
 		end,
 	},
+	-- {
+	-- 	"Exafunction/codeium.nvim",
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"hrsh7th/nvim-cmp",
+	-- 	},
+	-- 	config = function()
+	-- 		require("codeium").setup({})
+	-- 	end,
+	-- },
 	{
-		"Exafunction/codeium.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"hrsh7th/nvim-cmp",
-		},
-		config = function()
-			require("codeium").setup({})
-		end,
+		"github/copilot.vim",
 	},
 }
 
