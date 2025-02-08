250de45436bdcae1e0c1d00ffff52f8d5cfbe3da
arpit <arpit2003y@gmail.com>
2025-02-09 00:47:30 +0530

    feat(lsp):added_vue_lsp_support

--- lua/drxvim/plugins/lsp/lspconfig.lua
+++ lua/drxvim/plugins/lsp/lspconfig.lua
@@ -470,10 +470,59 @@ lspconfig.svelte.setup({
 	root_dir = util.root_pattern("package.json", ".git"),
 })
 
+-- deno language server configuration--
 lspconfig.denols.setup({
 	on_attach = M.on_attach,
 	capabilities = M.capabilities,
 	root_dir = util.root_pattern("deno.json", "deno.jsonc"),
+})
+
+-- vue language server configuration --
+
+lspconfig.vuels.setup({
+	on_attach = M.on_attach,
+	capabilities = M.capabilities,
+	cmd = { "vls" },
+	filetypes = { "vue" },
+	root_dir = util.root_pattern("package.json", ".git"),
+	init_options = {
+		config = {
+			css = {},
+			emmet = {},
+			html = {
+				suggest = {},
+			},
+			javascript = {
+				format = {},
+			},
+			stylusSupremacy = {},
+			typescript = {
+				format = {},
+			},
+			vetur = {
+				completion = {
+					autoImport = false,
+					tagCasing = "kebab",
+					useScaffoldSnippets = false,
+				},
+				format = {
+					defaultFormatter = {
+						js = "none",
+						ts = "none",
+					},
+					defaultFormatterOptions = {},
+					scriptInitialIndent = false,
+					styleInitialIndent = false,
+				},
+				useWorkspaceDependencies = false,
+				validation = {
+					script = true,
+					style = true,
+					template = true,
+				},
+			},
+		},
+	},
 })
 
 return M
