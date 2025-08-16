2145357cd31bf8c834059edb47d35e53f0df39f6
arpit <arpit2003y@gmail.com>
2025-08-16 17:51:55 +0530

    fix(snippets):snippets_working

--- lua/drxvim/plugins/cmp/snippets.lua
+++ lua/drxvim/plugins/cmp/snippets.lua
@@ -3,7 +3,6 @@ local snippets = require "mini.snippets"
 local M = {}
 
 M.snippets = {
-	snippets.gen_loader.from_file "~/.config/nvim/snippets/global.json",
 	snippets.gen_loader.from_lang(),
 }
 
