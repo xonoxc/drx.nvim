9aec119a5604df64411c98a7e65a63e58785e842
arpit <arpit2003y@gmail.com>
2025-09-09 14:13:00 +0530

    fix(diagnotics):diagnostics_fix

--- lua/drxvim/plugins/lsp/lspconfig.lua
+++ lua/drxvim/plugins/lsp/lspconfig.lua
@@ -28,9 +28,11 @@ M.capabilities.offsetEncoding = { "utf-16", "utf-8" }
 -- Diagnostic configuration for Neovim LSP diagnostics
 vim.diagnostic.config {
 	virtual_text = false,
+	virtual_lines = true,
 	underline = true,
 	update_in_insert = false,
 	severity_sort = true,
+	float = false,
 	signs = {
 		text = {
 			[vim.diagnostic.severity.ERROR] = " ",
@@ -39,14 +41,14 @@ vim.diagnostic.config {
 			[vim.diagnostic.severity.HINT] = "󰛨 ",
 		},
 	},
-	float = {
+	--[[ float = {
 		focusable = false,
 		suffix = "",
 		header = { "  Diagnostics", "String" },
 		prefix = function(_, _, _)
 			return "  ", "String"
 		end,
-	},
+	}, ]]
 }
 
 -- CONFIGS ---
